024f09e7094b7826c765c23a4fe63d1bb0dc644e
mushfiq <mushfiqxrabbi@gmail.com>
2024-12-21 01:52:55 +0600

    change some keymaps in neocodeium and blink

--- lua/plugins/modified/blink.lua
+++ lua/plugins/modified/blink.lua
@@ -4,9 +4,13 @@ return {
     keymap = {
       preset = "enter",
       ["<C-space>"] = { "show", "hide", "fallback" },
-      ["<C-e>"] = { "show_documentation", "hide_documentation", "fallback" },
+      ["<C-e>"] = {},
+      ["<A-e>"] = { "show_documentation", "hide_documentation", "fallback" },
     },
     completion = {
+      documentation = {
+        auto_show = false,
+      },
       ghost_text = {
         enabled = false,
       },
