3dc71a1d2cb2811b97ffe2074455a4bbcf03d28f
mushfiq <mushfiqxrabbi@gmail.com>
2024-12-21 04:51:52 +0600

    modify some keymaps in fzf.lua

--- lua/plugins/modified/fzf.lua
+++ lua/plugins/modified/fzf.lua
@@ -8,7 +8,17 @@ return {
     { "<leader>/", "<cmd>FzfLua grep_curbuf<cr>", desc = "Search in Buffer" },
   },
   opts = function(_, opts)
+    local config = require("fzf-lua.config")
+
     opts[1] = "borderless_full"
     opts.fzf_opts["--no-scrollbar"] = false
+
+    config.defaults.keymap.builtin["<c-f>"] = "preview-down"
+    config.defaults.keymap.builtin["<c-b>"] = "preview-up"
+    config.defaults.keymap.builtin["<c-p>"] = "toggle-preview"
+    config.defaults.keymap.builtin["<c-h>"] = "toggle-help"
+    config.defaults.keymap.builtin["<M-f>"] = "toggle-fullscreen"
+    config.defaults.keymap.builtin["<M-w>"] = "toggle-preview-wrap"
+    config.defaults.keymap.fzf["ctrl-u"] = "unix-line-discard"
   end,
 }
