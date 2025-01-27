a0cbcb1e15f9f2cb7efcd127673b9a8adea22437
mushfiq <mushfiqxrabbi@gmail.com>
2025-01-28 01:02:20 +0600

    feat: add vim.rooter plugin
      - add vim.rooter plugin to make neotree change directory auto.

---
+++ lua/plugins/added/rooter.lua
@@ -0,0 +1,10 @@
+return {
+  "airblade/vim-rooter",
+  event = "VeryLazy",
+  config = function()
+    vim.cmd([[
+      let g:rooter_patterns += ['.root']
+    ]])
+    vim.g.rooter_silent_chdir = 1
+  end,
+}
