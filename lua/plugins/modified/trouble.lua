aac095779c79c491ec4462a24e5cc88b8e2de6c9
mushfiq <mushfiqxrabbi@gmail.com>
2025-02-04 01:20:41 +0600

    update(keymap): update some trouble.nvim keymaps

---
+++ lua/plugins/modified/trouble.lua
@@ -0,0 +1,7 @@
+return {
+  "folke/trouble.nvim",
+  keys = {
+    { "<leader>xx", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>", desc = "Buffer Diagnostics (Trouble)" },
+    { "<leader>xX", "<cmd>Trouble diagnostics toggle<cr>", desc = "Diagnostics (Trouble)" },
+  },
+}
