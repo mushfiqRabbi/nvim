3e1bb05b73aec57abfcc0ec4e53ebf92463371be
mushfiq <mushfiqxrabbi@gmail.com>
2025-02-06 05:40:56 +0600

    feat: disable avante.nvim

--- lua/plugins/added/avante.lua
+++ lua/plugins/added/avante.lua
@@ -1,6 +1,6 @@
 return {
   "yetone/avante.nvim",
-  -- enabled = false,
+  enabled = false,
   event = "VeryLazy",
   lazy = false,
   version = false, -- set this if you want to always pull the latest change
