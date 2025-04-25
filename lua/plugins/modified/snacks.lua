c3341c0d625be9cbf6d2c0de9307d9e05cfaf6de
mushfiq <mushfiqxrabbi@gmail.com>
2025-04-25 07:42:12 +0600

    feat(snacks): add dynamic layout configuration for pickers

--- lua/plugins/modified/snacks.lua
+++ lua/plugins/modified/snacks.lua
@@ -101,12 +101,69 @@ return {
     },
     lazygit = {
       config = {
-        -- gui = {
-        --   border = "single",
-        -- },
+        gui = {
+          theme = {
+            inactiveBorderColor = { "#45475A" },
+          },
+        },
       },
     },
     picker = {
+      layout = {
+        -- The default layout for "telescopy" pickers, e.g. `files`, `commands`, ...
+        -- It will not override non-standard pickers, e.g. `explorer`, `lines`, ...
+        preset = function()
+          return vim.o.columns >= 120 and "telescope" or "vertical"
+        end,
+      },
+      layouts = {
+        telescope = {
+          -- Copy from https://github.com/folke/snacks.nvim/blob/main/docs/picker.md#telescope
+          reverse = false,
+          layout = {
+            box = "horizontal",
+            backdrop = false,
+            width = 0.8, -- Change the width
+            height = 0.9,
+            border = "none",
+            {
+              box = "vertical",
+              {
+                win = "input",
+                height = 1,
+                border = "rounded",
+                title = "{title} {live} {flags}",
+                title_pos = "center",
+              },
+              { win = "list", title = " Results ", title_pos = "center", border = "rounded" },
+            },
+            {
+              win = "preview",
+              title = "{preview:Preview}",
+              width = 0.45, -- Change the preview width
+              border = "rounded",
+              title_pos = "center",
+            },
+          },
+        },
+      },
+      -- sources = {
+      --   files = {},
+      --   explorer = {
+      --     layout = {
+      --       layout = {
+      --         position = "right",
+      --       },
+      --     },
+      --   },
+      --   lines = {
+      --     layout = {
+      --       preset = function()
+      --         return vim.o.columns >= 120 and "telescope" or "vertical"
+      --       end,
+      --     },
+      --   },
+      -- },
       previewers = {
         git = {
           builtin = false,
--- lua/plugins/modified/tokyonight.lua
+++ lua/plugins/modified/tokyonight.lua
@@ -9,12 +9,88 @@ return {
         fg = "#292e44", -- 30% shade
       }
       hl.IndentLight = {
-        fg = "#4f5571", -- 10% tint
+        fg = "#353b57", -- 10% shade
       }
       hl.MatchParen = {
         bold = true,
         fg = "#ff9e64",
         bg = "#545868", -- 20% tint
+      }
+
+      hl.SnacksPickerToggleRegex = {
+        bold = true,
+        italic = true,
+        fg = "#1f2335",
+        bg = "#c65e72",
+      }
+
+      hl.SnacksPickerToggleHidden = {
+        bold = true,
+        italic = true,
+        fg = "#1f2335",
+        bg = "#c65e72",
+      }
+
+      hl.SnacksPickerToggleFollow = {
+        bold = true,
+        italic = true,
+        fg = "#1f2335",
+        bg = "#c65e72",
+      }
+
+      hl.SnacksPickerToggleIgnored = {
+        bold = true,
+        italic = true,
+        fg = "#1f2335",
+        bg = "#c65e72",
+      }
+      hl.SnacksPickerToggleModified = {
+        bold = true,
+        italic = true,
+        fg = "#1f2335",
+        bg = "#c65e72",
+      }
+
+      hl.SnacksPickerBorder = {
+        fg = "#45475A",
+        bg = "NONE",
+      }
+
+      hl.SnacksPickerTitle = {
+        bg = "#7aa2f7",
+        fg = "#1f2335",
+      }
+
+      hl.SnacksPickerPreview = {
+        bg = "#1a1b26",
+      }
+
+      hl.SnacksPickerList = {
+        bg = "#1a1b26",
+      }
+
+      hl.SnacksPickerListTitle = {
+        bg = "#9ece6a",
+        fg = "#1f2335",
+      }
+
+      hl.SnacksPickerInputTitle = {
+        bg = "#f7768e",
+        fg = "#1f2335",
+      }
+
+      hl.SnacksPickerInputBorder = {
+        bg = "#1a1b26",
+        fg = "#45475a",
+      }
+
+      hl.SnacksPickerInputSearch = {
+        bg = "#f7768e",
+        fg = "#1f2335",
+      }
+
+      hl.SnacksPickerInput = {
+        bg = "#1a1b26",
       }
     end,
   },
