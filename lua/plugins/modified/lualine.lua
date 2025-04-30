d4deaba9fcb494f70fd1c03d1c1e8a68764ee0aa
mushfiq <mushfiqxrabbi@gmail.com>
2025-05-01 05:21:04 +0600

    style(lualine): update mode and location styling

--- lua/plugins/modified/lualine.lua
+++ lua/plugins/modified/lualine.lua
@@ -12,17 +12,20 @@ return {
 
     opts.options.component_separators = "│"
     opts.options.section_separators = ""
-    opts.sections.lualine_a = { {
-      "mode",
-      icon_enabled = true,
-      icon = "",
-    } }
+    opts.sections.lualine_a = {
+      {
+        "mode",
+        color = { gui = "bold" },
+        -- icon_enabled = true,
+        -- icon = "",
+      },
+    }
     table.insert(opts.sections.lualine_x, { lint_progress })
     opts.sections.lualine_y = {
       { "progress" },
     }
     opts.sections.lualine_z = {
-      { "location" },
+      { "location", color = { gui = "bold" } },
     }
   end,
 }
