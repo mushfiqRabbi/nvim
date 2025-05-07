2ebe3537184e3532290a7036dd37b4d066e5d763
mushfiq <mushfiqxrabbi@gmail.com>
2025-05-08 02:38:35 +0600

    refactor(plugins): remove mason-workaround.lua

--- lua/plugins/modified/grug-far.lua
+++ lua/plugins/modified/grug-far.lua
@@ -19,11 +19,18 @@ return {
       "<leader>sR",
       function()
         local grug = require("grug-far")
-        local ext = vim.bo.buftype == "" and vim.fn.expand("%:e")
+        -- local ext = vim.bo.buftype == "" and vim.fn.expand("%:e")
+        -- grug.open({
+        --   transient = true,
+        --   prefills = {
+        --     filesFilter = ext and ext ~= "" and "*." .. ext or nil,
+        --   },
+        -- })
+
         grug.open({
           transient = true,
           prefills = {
-            filesFilter = ext and ext ~= "" and "*." .. ext or nil,
+            paths = LazyVim.root(),
           },
         })
       end,
---
+++ lua/plugins/workarounds.lua
@@ -0,0 +1,10 @@
+local workarounds_needed = true
+
+if not workarounds_needed then
+  return {}
+end
+
+return {
+  { "mason-org/mason.nvim", version = "^1.0.0" },
+  { "mason-org/mason-lspconfig.nvim", version = "^1.0.0" },
+}
