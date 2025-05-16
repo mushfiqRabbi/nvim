07640018f41a090f167316aecf778778716d96dc
mushfiq <mushfiqxrabbi@gmail.com>
2025-05-16 07:41:10 +0600

    feat(multicursor): simplify keybindings and enable plugin
    - Enable multicursor.nvim plugin by uncommenting `enabled = false`
    - Remove most keybindings, keeping only essential ones:
      - Change word matching from `<leader>n/N` to `<c-n>/<c-p>`
      - Keep mouse controls, toggle, and visual mode operations
      - Keep global operations and diagnostics
    - Disable visual-multi plugin by setting `enabled = false`
    - Comment out unused highlight group configurations

--- lua/plugins/added/multicursor.lua
+++ lua/plugins/added/multicursor.lua
@@ -1,76 +1,76 @@
 return {
   "jake-stewart/multicursor.nvim",
-  enabled = false,
+  -- enabled = false,
   event = "VeryLazy",
   branch = "1.0",
   keys = {
     -- Line-based cursors
-    {
-      "<up>",
-      function()
-        require("multicursor-nvim").lineAddCursor(-1)
-      end,
-      mode = { "n", "x" },
-      desc = "Add cursor above",
-    },
-    {
-      "<down>",
-      function()
-        require("multicursor-nvim").lineAddCursor(1)
-      end,
-      mode = { "n", "x" },
-      desc = "Add cursor below",
-    },
-    {
-      "<leader><up>",
-      function()
-        require("multicursor-nvim").lineSkipCursor(-1)
-      end,
-      mode = { "n", "x" },
-      desc = "Skip and add cursor above",
-    },
-    {
-      "<leader><down>",
-      function()
-        require("multicursor-nvim").lineSkipCursor(1)
-      end,
-      mode = { "n", "x" },
-      desc = "Skip and add cursor below",
-    },
+    -- {
+    --   "<up>",
+    --   function()
+    --     require("multicursor-nvim").lineAddCursor(-1)
+    --   end,
+    --   mode = { "n", "x" },
+    --   desc = "Add cursor above",
+    -- },
+    -- {
+    --   "<down>",
+    --   function()
+    --     require("multicursor-nvim").lineAddCursor(1)
+    --   end,
+    --   mode = { "n", "x" },
+    --   desc = "Add cursor below",
+    -- },
+    -- {
+    --   "<leader><up>",
+    --   function()
+    --     require("multicursor-nvim").lineSkipCursor(-1)
+    --   end,
+    --   mode = { "n", "x" },
+    --   desc = "Skip and add cursor above",
+    -- },
+    -- {
+    --   "<leader><down>",
+    --   function()
+    --     require("multicursor-nvim").lineSkipCursor(1)
+    --   end,
+    --   mode = { "n", "x" },
+    --   desc = "Skip and add cursor below",
+    -- },
 
     -- Word/selection matching
     {
-      "<leader>n",
+      "<c-n>",
       function()
         require("multicursor-nvim").matchAddCursor(1)
       end,
       mode = { "n", "x" },
       desc = "Add cursor to next match",
     },
+    -- {
+    --   "<leader>s",
+    --   function()
+    --     require("multicursor-nvim").matchSkipCursor(1)
+    --   end,
+    --   mode = { "n", "x" },
+    --   desc = "Skip to next match",
+    -- },
     {
-      "<leader>s",
-      function()
-        require("multicursor-nvim").matchSkipCursor(1)
-      end,
-      mode = { "n", "x" },
-      desc = "Skip to next match",
-    },
-    {
-      "<leader>N",
+      "<c-p>",
       function()
         require("multicursor-nvim").matchAddCursor(-1)
       end,
       mode = { "n", "x" },
       desc = "Add cursor to previous match",
     },
-    {
-      "<leader>S",
-      function()
-        require("multicursor-nvim").matchSkipCursor(-1)
-      end,
-      mode = { "n", "x" },
-      desc = "Skip to previous match",
-    },
+    -- {
+    --   "<leader>S",
+    --   function()
+    --     require("multicursor-nvim").matchSkipCursor(-1)
+    --   end,
+    --   mode = { "n", "x" },
+    --   desc = "Skip to previous match",
+    -- },
 
     -- Mouse controls
     {
@@ -107,14 +107,14 @@ return {
       mode = { "n", "x" },
       desc = "Toggle cursors",
     },
-    {
-      "<leader><c-q>",
-      function()
-        require("multicursor-nvim").duplicateCursors()
-      end,
-      mode = { "n", "x" },
-      desc = "Duplicate cursors",
-    },
+    -- {
+    --   "<leader><c-q>",
+    --   function()
+    --     require("multicursor-nvim").duplicateCursors()
+    --   end,
+    --   mode = { "n", "x" },
+    --   desc = "Duplicate cursors",
+    -- },
     {
       "<leader>gv",
       function()
@@ -125,32 +125,32 @@ return {
     },
 
     -- Operators
-    {
-      "ga",
-      function()
-        require("multicursor-nvim").addCursorOperator()
-      end,
-      mode = "n",
-      desc = "Add cursors (operator)",
-    },
-    {
-      "<leader>m",
-      function()
-        require("multicursor-nvim").operator()
-      end,
-      mode = { "n", "x" },
-      desc = "Custom operator",
-    },
+    -- {
+    --   "ga",
+    --   function()
+    --     require("multicursor-nvim").addCursorOperator()
+    --   end,
+    --   mode = "n",
+    --   desc = "Add cursors (operator)",
+    -- },
+    -- {
+    --   "<leader>m",
+    --   function()
+    --     require("multicursor-nvim").operator()
+    --   end,
+    --   mode = { "n", "x" },
+    --   desc = "Custom operator",
+    -- },
 
     -- Visual mode operations
-    {
-      "S",
-      function()
-        require("multicursor-nvim").splitCursors()
-      end,
-      mode = "x",
-      desc = "Split visual selection",
-    },
+    -- {
+    --   "S",
+    --   function()
+    --     require("multicursor-nvim").splitCursors()
+    --   end,
+    --   mode = "x",
+    --   desc = "Split visual selection",
+    -- },
     {
       "M",
       function()
@@ -177,90 +177,90 @@ return {
     },
 
     -- Alignment and transformation
-    {
-      "<leader>a",
-      function()
-        require("multicursor-nvim").alignCursors()
-      end,
-      mode = "n",
-      desc = "Align cursors",
-    },
-    {
-      "<leader>t",
-      function()
-        require("multicursor-nvim").transposeCursors(1)
-      end,
-      mode = "x",
-      desc = "Rotate selections forward",
-    },
-    {
-      "<leader>T",
-      function()
-        require("multicursor-nvim").transposeCursors(-1)
-      end,
-      mode = "x",
-      desc = "Rotate selections backward",
-    },
+    -- {
+    --   "<leader>a",
+    --   function()
+    --     require("multicursor-nvim").alignCursors()
+    --   end,
+    --   mode = "n",
+    --   desc = "Align cursors",
+    -- },
+    -- {
+    --   "<leader>t",
+    --   function()
+    --     require("multicursor-nvim").transposeCursors(1)
+    --   end,
+    --   mode = "x",
+    --   desc = "Rotate selections forward",
+    -- },
+    -- {
+    --   "<leader>T",
+    --   function()
+    --     require("multicursor-nvim").transposeCursors(-1)
+    --   end,
+    --   mode = "x",
+    --   desc = "Rotate selections backward",
+    -- },
 
     -- Sequences
-    {
-      "g<c-a>",
-      function()
-        require("multicursor-nvim").sequenceIncrement()
-      end,
-      mode = { "n", "x" },
-      desc = "Increment sequence",
-    },
-    {
-      "g<c-x>",
-      function()
-        require("multicursor-nvim").sequenceDecrement()
-      end,
-      mode = { "n", "x" },
-      desc = "Decrement sequence",
-    },
-
-    -- Search-based
-    {
-      "<leader>/n",
-      function()
-        require("multicursor-nvim").searchAddCursor(1)
-      end,
-      mode = "n",
-      desc = "Add cursor to next search",
-    },
-    {
-      "<leader>/N",
-      function()
-        require("multicursor-nvim").searchAddCursor(-1)
-      end,
-      mode = "n",
-      desc = "Add cursor to prev search",
-    },
-    {
-      "<leader>/s",
-      function()
-        require("multicursor-nvim").searchSkipCursor(1)
-      end,
-      mode = "n",
-      desc = "Skip to next search",
-    },
-    {
-      "<leader>/S",
-      function()
-        require("multicursor-nvim").searchSkipCursor(-1)
-      end,
-      mode = "n",
-      desc = "Skip to prev search",
-    },
-    {
-      "<leader>/A",
-      function()
-        require("multicursor-nvim").searchAllAddCursors()
-      end,
-      mode = "n",
-      desc = "Add cursors to all matches",
-    },
+    -- {
+    --   "g<c-a>",
+    --   function()
+    --     require("multicursor-nvim").sequenceIncrement()
+    --   end,
+    --   mode = { "n", "x" },
+    --   desc = "Increment sequence",
+    -- },
+    -- {
+    --   "g<c-x>",
+    --   function()
+    --     require("multicursor-nvim").sequenceDecrement()
+    --   end,
+    --   mode = { "n", "x" },
+    --   desc = "Decrement sequence",
+    -- },
+    --
+    -- -- Search-based
+    -- {
+    --   "<leader>/n",
+    --   function()
+    --     require("multicursor-nvim").searchAddCursor(1)
+    --   end,
+    --   mode = "n",
+    --   desc = "Add cursor to next search",
+    -- },
+    -- {
+    --   "<leader>/N",
+    --   function()
+    --     require("multicursor-nvim").searchAddCursor(-1)
+    --   end,
+    --   mode = "n",
+    --   desc = "Add cursor to prev search",
+    -- },
+    -- {
+    --   "<leader>/s",
+    --   function()
+    --     require("multicursor-nvim").searchSkipCursor(1)
+    --   end,
+    --   mode = "n",
+    --   desc = "Skip to next search",
+    -- },
+    -- {
+    --   "<leader>/S",
+    --   function()
+    --     require("multicursor-nvim").searchSkipCursor(-1)
+    --   end,
+    --   mode = "n",
+    --   desc = "Skip to prev search",
+    -- },
+    -- {
+    --   "<leader>/A",
+    --   function()
+    --     require("multicursor-nvim").searchAllAddCursors()
+    --   end,
+    --   mode = "n",
+    --   desc = "Add cursors to all matches",
+    -- },
 
     -- Global operations
     {
@@ -273,46 +273,48 @@ return {
     },
 
     -- Diagnostics
-    {
-      "]d",
-      function()
-        require("multicursor-nvim").diagnosticAddCursor(1)
-      end,
-      mode = { "n", "x" },
-      desc = "Add cursor to next diagnostic",
-    },
-    {
-      "[d",
-      function()
-        require("multicursor-nvim").diagnosticAddCursor(-1)
-      end,
-      mode = { "n", "x" },
-      desc = "Add cursor to prev diagnostic",
-    },
-    {
-      "]s",
-      function()
-        require("multicursor-nvim").diagnosticSkipCursor(1)
-      end,
-      mode = { "n", "x" },
-      desc = "Skip to next diagnostic",
-    },
-    {
-      "[S",
-      function()
-        require("multicursor-nvim").diagnosticSkipCursor(-1)
-      end,
-      mode = { "n", "x" },
-      desc = "Skip to prev diagnostic",
-    },
-    {
-      "md",
-      function()
-        require("multicursor-nvim").diagnosticMatchCursors({ severity = vim.diagnostic.severity.ERROR })
-      end,
-      mode = { "n", "x" },
-      desc = "Add cursors to diagnostics in range",
-    },
+    -- {
+    --   "]d",
+    --   function()
+    --     require("multicursor-nvim").diagnosticAddCursor(1)
+    --   end,
+    --   mode = { "n", "x" },
+    --   desc = "Add cursor to next diagnostic",
+    -- },
+    -- {
+    --   "[d",
+    --   function()
+    --     require("multicursor-nvim").diagnosticAddCursor(-1)
+    --   end,
+    --   mode = { "n", "x" },
+    --   desc = "Add cursor to prev diagnostic",
+    -- },
+    -- {
+    --   "]s",
+    --   function()
+    --     require("multicursor-nvim").diagnosticSkipCursor(1)
+    --   end,
+    --   mode = { "n", "x" },
+    --   desc = "Skip to next diagnostic",
+    -- },
+    -- {
+    --   "[S",
+    --   function()
+    --     require("multicursor-nvim").diagnosticSkipCursor(-1)
+    --   end,
+    --   mode = { "n", "x" },
+    --   desc = "Skip to prev diagnostic",
+    -- },
+    -- {
+    --   "md",
+    --   function()
+    --     require("multicursor-nvim").diagnosticMatchCursors({
+    --       severity = vim.diagnostic.severity.ERROR,
+    --     })
+    --   end,
+    --   mode = { "n", "x" },
+    --   desc = "Add cursors to diagnostics in range",
+    -- },
   },
   config = function()
     local mc = require("multicursor-nvim")
@@ -461,13 +463,13 @@ return {
     end)
 
     -- Customize how cursors look.
-    local hl = vim.api.nvim_set_hl
-    hl(0, "MultiCursorCursor", { link = "Cursor" })
-    hl(0, "MultiCursorVisual", { link = "Visual" })
-    hl(0, "MultiCursorSign", { link = "SignColumn" })
-    hl(0, "MultiCursorMatchPreview", { link = "Search" })
-    hl(0, "MultiCursorDisabledCursor", { link = "Visual" })
-    hl(0, "MultiCursorDisabledVisual", { link = "Visual" })
-    hl(0, "MultiCursorDisabledSign", { link = "SignColumn" })
+    -- local hl = vim.api.nvim_set_hl
+    -- hl(0, "MultiCursorCursor", { link = "Cursor" })
+    -- hl(0, "MultiCursorVisual", { link = "Visual" })
+    -- hl(0, "MultiCursorSign", { link = "SignColumn" })
+    -- hl(0, "MultiCursorMatchPreview", { link = "Search" })
+    -- hl(0, "MultiCursorDisabledCursor", { link = "Visual" })
+    -- hl(0, "MultiCursorDisabledVisual", { link = "Visual" })
+    -- hl(0, "MultiCursorDisabledSign", { link = "SignColumn" })
   end,
 }
