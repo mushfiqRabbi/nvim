ed86a312a384fbe9c074d571ad0ef15e115f5885
mushfiq <mushfiqxrabbi@gmail.com>
2025-05-29 03:27:23 +0600

    feat: add lazydev.nvim plugin for development utilities
    - Added lazydev.nvim plugin
    - Configured plugin with wezterm-types dependency
    - Added wezterm library path for type hinting

---
+++ lua/plugins/modified/lazydev.lua
@@ -0,0 +1,11 @@
+return {
+  "folke/lazydev.nvim",
+  dependencies = {
+    { "gonstoll/wezterm-types", lazy = true },
+  },
+  opts = {
+    library = {
+      { path = "wezterm-types", mods = { "wezterm" } },
+    },
+  },
+}
