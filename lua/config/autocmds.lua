460fe58087942f816dcfc0f305352cf474fbbc68
mushfiq <mushfiqxrabbi@gmail.com>
2025-06-01 02:25:06 +0600

    feat: remove kitty terminal integration autocmds
    - Removed autocmds for setting and unsetting kitty terminal variables
      - The feature is no longer needed.

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -204,17 +204,3 @@ vim.api.nvim_create_autocmd("LspDetach", {
     end
   end,
 })
-
-vim.api.nvim_create_autocmd({ "VimEnter", "VimResume" }, {
-  group = vim.api.nvim_create_augroup("KittySetVarVimEnter", { clear = true }),
-  callback = function()
-    io.stdout:write("\x1b]1337;SetUserVar=in_nvim=MQo\007")
-  end,
-})
-
-vim.api.nvim_create_autocmd({ "VimLeave", "VimSuspend" }, {
-  group = vim.api.nvim_create_augroup("KittyUnsetVarVimLeave", { clear = true }),
-  callback = function()
-    io.stdout:write("\x1b]1337;SetUserVar=in_nvim\007")
-  end,
-})
