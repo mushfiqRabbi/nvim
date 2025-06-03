2fdd640d64bf9298e02915066fc407a144a23788
mushfiq <mushfiqxrabbi@gmail.com>
2025-06-03 10:18:54 +0600

    feat: add kitty-scrollback plugin
    - Added kitty-scrollback plugin for scrollback functionality.

--- lua/plugins/added/kitty-scrollback.lua
+++ lua/plugins/added/kitty-scrollback.lua
@@ -8,7 +8,7 @@ return {
     "KittyScrollbackGenerateCommandLineEditing",
   },
   event = { "User KittyScrollbackLaunch" },
-  version = "*", -- latest stable version, may have breaking changes if major version changed
+  -- version = '*', -- latest stable version, may have breaking changes if major version changed
   -- version = '^6.0.0', -- pin major version, include fixes and features that do not have breaking changes
   config = function()
     require("kitty-scrollback").setup()
