e10592a69c7a129a6a875f98b848454d7d249c93
mushfiq <mushfiqxrabbi@gmail.com>
2025-06-06 03:12:04 +0600

    feat: remove kitty-scrollback plugin
    This commit removes the kitty-scrollback plugin as it's no longer needed.

--- lua/plugins/modified/tokyonight.lua
+++ lua/plugins/modified/tokyonight.lua
@@ -1,98 +1,21 @@
 return {
   "folke/tokyonight.nvim",
   opts = {
-    style = "night",
+    style = "moon",
     ---@diagnostic disable-next-line: unused-local
     on_highlights = function(hl, c)
-      -- NOTE: shade and tint from https://maketintsandshades.com/#3b4261
+      -- NOTE: shade and tint from https://noeldelgado.github.io/shadowlord/#3b4261
       hl.IndentDark = {
-        fg = "#292e44", -- 30% shade
+        fg = "#323852",
       }
       hl.IndentLight = {
-        fg = "#4f5571", -- 10% tint
-        -- fg = "#353b57", -- 10% shade
+        fg = "#585e79",
       }
       hl.MatchParen = {
         bold = true,
-        fg = "#ff9e64",
-        bg = "#545868", -- 20% tint
+        fg = "#ffc777",
+        bg = "#636da6",
       }
-
-      -- hl.SnacksPickerBorder = {
-      --   fg = "#45475A",
-      --   bg = "NONE",
-      -- }
-      --
-      -- hl.SnacksPickerTitle = {
-      --   bg = "#7aa2f7",
-      --   fg = "#1f2335",
-      -- }
-      --
-      -- hl.SnacksPickerPreview = {
-      --   bg = "#1a1b26",
-      -- }
-      --
-      -- hl.SnacksPickerList = {
-      --   bg = "#1a1b26",
-      -- }
-      --
-      -- hl.SnacksPickerListTitle = {
-      --   bg = "#9ece6a",
-      --   fg = "#1f2335",
-      -- }
-      --
-      -- hl.SnacksPickerInputTitle = {
-      --   bg = "#f7768e",
-      --   fg = "#1f2335",
-      -- }
-      --
-      -- hl.SnacksPickerInputBorder = {
-      --   bg = "#1a1b26",
-      --   fg = "#45475a",
-      -- }
-      --
-      -- hl.SnacksPickerInputSearch = {
-      --   bg = "#f7768e",
-      --   fg = "#1f2335",
-      -- }
-      --
-      -- hl.SnacksPickerInput = {
-      --   bg = "#1a1b26",
-      -- }
-      --
-      -- hl.SnacksPickerToggleRegex = {
-      --   bold = true,
-      --   italic = true,
-      --   fg = "#1f2335",
-      --   bg = "#c65e72",
-      -- }
-      --
-      -- hl.SnacksPickerToggleHidden = {
-      --   bold = true,
-      --   italic = true,
-      --   fg = "#1f2335",
-      --   bg = "#c65e72",
-      -- }
-      --
-      -- hl.SnacksPickerToggleFollow = {
-      --   bold = true,
-      --   italic = true,
-      --   fg = "#1f2335",
-      --   bg = "#c65e72",
-      -- }
-      --
-      -- hl.SnacksPickerToggleIgnored = {
-      --   bold = true,
-      --   italic = true,
-      --   fg = "#1f2335",
-      --   bg = "#c65e72",
-      -- }
-      -- hl.SnacksPickerToggleModified = {
-      --   bold = true,
-      --   italic = true,
-      --   fg = "#1f2335",
-      --   bg = "#c65e72",
-      -- }
     end,
   },
 }
