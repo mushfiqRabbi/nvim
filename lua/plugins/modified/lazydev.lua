0c15d286de84f77cd0fe5634ac77bdbeec9cae9c
mushfiq <mushfiqxrabbi@gmail.com>
2025-06-17 06:41:49 +0600

    feat: remove vim-kitty plugin and disable lazydev
    - Remove vim-kitty plugin
    - Disable lazydev.nvim plugin

--- lua/plugins/modified/lazydev.lua
+++ lua/plugins/modified/lazydev.lua
@@ -1,11 +1,11 @@
 return {
-  "folke/lazydev.nvim",
-  dependencies = {
-    { "gonstoll/wezterm-types", lazy = true },
-  },
-  opts = {
-    library = {
-      { path = "wezterm-types", mods = { "wezterm" } },
-    },
-  },
+  -- "folke/lazydev.nvim",
+  -- dependencies = {
+  --   { "gonstoll/wezterm-types", lazy = true },
+  -- },
+  -- opts = {
+  --   library = {
+  --     { path = "wezterm-types", mods = { "wezterm" } },
+  --   },
+  -- },
 }
