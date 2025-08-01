8849d63f18883b3c826b26868e0dfa252b27ffc6
mushfiq <mushfiqxrabbi@gmail.com>
2025-08-02 00:12:53 +0600

    🔧 feat(dooing.lua): customize window width and status icons
    This change updates the configuration for the `dooing` plugin to improve
    its visual presentation and usability.
    
    The window width is set to 100 to provide more horizontal space,
    preventing long todo items from wrapping and thus improving readability.
    Custom Nerd Font icons are configured for `pending`, `in-progress`, and
    `done` statuses to offer a clearer and more intuitive visual
    representation of each task's state.

--- lua/plugins/added/dooing.lua
+++ lua/plugins/added/dooing.lua
@@ -4,6 +4,20 @@ return {
   config = function()
     require("dooing").setup({
       save_path = vim.fn.expand("~") .. "/.dooing_global.json",
+      window = {
+        width = 100,
+      },
+      formatting = {
+        pending = {
+          icon = " ",
+        },
+        in_progress = {
+          icon = " ",
+        },
+        done = {
+          icon = " ",
+        },
+      },
       quick_keys = false,
       per_project = {
         default_filename = ".dooing_local.json",
