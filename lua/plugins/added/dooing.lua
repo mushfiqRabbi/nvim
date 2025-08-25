cd6ac7c75d862451c774cd9bba41a04876383504
mushfiq <mushfiqxrabbi@gmail.com>
2025-08-26 00:59:40 +0600

    🔧 feat(dooing): configure plugin settings and keymaps
      - Disable timestamp feature in global config
      - Set nested task indent to 3 spaces
      - Add keymap 'd' for deleting todos
      - Maintain existing window and file settings

--- lua/plugins/added/dooing.lua
+++ lua/plugins/added/dooing.lua
@@ -4,6 +4,9 @@ return {
   config = function()
     require("dooing").setup({
       save_path = vim.fn.expand("~") .. "/.dooing_global.json",
+      timestamp = {
+        enabled = false,
+      },
       window = {
         width = 100,
       },
@@ -23,9 +26,13 @@ return {
         default_filename = ".dooing_local.json",
         on_missing = "auto_create",
       },
+      nested_tasks = {
+        indent = 3,
+      },
       keymaps = {
         toggle_window = "<leader>tg",
         open_project_todo = "<leader>tl",
+        delete_todo = "d",
       },
     })
   end,
