8c2c5db2f593fcc09566b269d892a2b148d75991
mushfiq <mushfiqxrabbi@gmail.com>
2025-10-01 16:09:04 +0600

    🔧 config(plugins): update blink completion settings
      - Remove unused <C-e> keymap
      - Configure completion menu with label and description columns
      - Adjust documentation auto show setting
      - Set max items for completion list
      - Disable ghost text feature

--- lua/plugins/modified/blink.lua
+++ lua/plugins/modified/blink.lua
@@ -7,46 +7,24 @@ return {
     keymap = {
       preset = "enter",
       ["<C-space>"] = { "show", "hide", "fallback" },
-      ["<C-e>"] = {},
+      -- ["<C-e>"] = {},
       ["<A-e>"] = { "show_documentation", "hide_documentation", "fallback" },
     },
     completion = {
-      -- menu = {
-      --   draw = {
-      --     -- We don't need label_description now because label and label_description are already
-      --     -- combined together in label by colorful-menu.nvim.
-      --     columns = { { "kind_icon" }, { "label", gap = 1 } },
-      --     components = {
-      --       label = {
-      --         -- width = { fill = true, max = 60 },
-      --         text = function(ctx)
-      --           local highlights_info = require("colorful-menu").blink_highlights(ctx)
-      --           if highlights_info ~= nil then
-      --             -- Or you want to add more item to label
-      --             return highlights_info.label
-      --           else
-      --             return ctx.label
-      --           end
-      --         end,
-      --         highlight = function(ctx)
-      --           local highlights = {}
-      --           local highlights_info = require("colorful-menu").blink_highlights(ctx)
-      --           if highlights_info ~= nil then
-      --             highlights = highlights_info.highlights
-      --           end
-      --           for _, idx in ipairs(ctx.label_matched_indices) do
-      --             table.insert(highlights, { idx, idx + 1, group = "BlinkCmpLabelMatch" })
-      --           end
-      --           -- Do something else
-      --           return highlights
-      --         end,
-      --       },
-      --     },
-      --   },
-      -- },
-      -- documentation = {
-      --   auto_show = true,
-      -- },
+      list = {
+        max_items = 7,
+      },
+      menu = {
+        draw = {
+          columns = {
+            { "label", "label_description", gap = 2 },
+            { "kind_icon", "kind" },
+          },
+        },
+      },
+      documentation = {
+        auto_show = false,
+      },
       ghost_text = {
         enabled = false,
       },
