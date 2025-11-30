2c2c49c6974323db41718ca72ee362c598d1292d
mushfiq <mushfiqxrabbi@gmail.com>
2025-11-30 07:19:54 +0600

    🔧 chore(supermaven): remove unused keymap configuration
      - Remove commented out accept_suggestion keymap

--- lua/plugins/added/supermaven.lua
+++ lua/plugins/added/supermaven.lua
@@ -4,7 +4,6 @@ return {
   config = function()
     require("supermaven-nvim").setup({
       keymaps = {
-        -- accept_suggestion = "<C-g>",
         clear_suggestion = "<C-]>",
         accept_word = "<C-[>",
       },
