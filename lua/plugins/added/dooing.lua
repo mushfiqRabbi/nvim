a4c220025d67e6de56fc5fd2733c4111bc699f35
mushfiq <mushfiqxrabbi@gmail.com>
2025-12-22 03:52:51 +0600

    ⚙️ refactor(dooing): update plugin config to use opts
      - Change from config function to opts table
      - Remove signal priority and add lowest priority group
      - Adjust priority weights and group configurations

--- lua/plugins/added/dooing.lua
+++ lua/plugins/added/dooing.lua
@@ -1,76 +1,58 @@
 return {
   "atiladefreitas/dooing",
   event = "VeryLazy",
-  config = function()
-    require("dooing").setup({
-      save_path = vim.fn.expand("~") .. "/.dooing_global.json",
-      timestamp = {
-        enabled = false,
+  opts = {
+    save_path = vim.fn.expand("~") .. "/.dooing_global.json",
+    timestamp = {
+      enabled = false,
+    },
+    window = {
+      width = 100,
+    },
+    formatting = {
+      pending = {
+        icon = " ",
       },
-      window = {
-        width = 100,
+      in_progress = {
+        icon = " ",
       },
-      formatting = {
-        pending = {
-          icon = " ",
-        },
-        in_progress = {
-          icon = " ",
-        },
-        done = {
-          icon = " ",
-        },
+      done = {
+        icon = " ",
       },
-      quick_keys = false,
-      notes = {
-        icon = "",
+    },
+    quick_keys = false,
+    notes = {
+      icon = "",
+    },
+    per_project = {
+      default_filename = ".dooing_local.json",
+      on_missing = "auto_create",
+    },
+    nested_tasks = {
+      indent = 3,
+    },
+    priorities = {
+      {
+        name = "important",
+        weight = 4,
       },
-      per_project = {
-        default_filename = ".dooing_local.json",
-        on_missing = "auto_create",
+      {
+        name = "urgent",
+        weight = 2,
       },
-      nested_tasks = {
-        indent = 3,
+      {
+        name = " ",
+        weight = -1,
       },
-      priorities = {
-        {
-          name = "signal",
-          weight = 12,
-        },
-        {
-          name = "important",
-          weight = 4,
-        },
-        {
-          name = "urgent",
-          weight = 2,
-        },
+    },
+    priority_groups = {
+      lowest = {
+        members = { " " },
+        hl_group = "IndentLight",
       },
-      priority_groups = {
-        max = {
-          members = {},
-          color = nil,
-          hl_group = "DiagnosticHint",
-        },
-        high = {
-          members = { "important", "urgent" },
-          color = nil,
-          hl_group = "DiagnosticError",
-        },
-        medium = {
-          members = { "important" },
-          color = nil,
-          hl_group = "DiagnosticWarn",
-        },
-        low = {
-          members = { "urgent" },
-          color = nil,
-          hl_group = "DiagnosticInfo",
-        },
-      },
-      keymaps = {
-        create_nested_task = "n",
-      },
-    })
-  end,
+    },
+    keymaps = {
+      create_nested_task = "n",
+    },
+  },
 }
